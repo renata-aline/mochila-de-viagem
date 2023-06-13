b46b21a4b91a9a2de6e09ef51a4ad717d93141a8
renata-aline <renataaline@gmail.com>
2023-06-13 17:07:16 -0300

    capiturando elemento e adicionando item na lista

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-const form = document.getElementById("novoItem")
+const form = document.getElementById("novoItem"); //captura formulário
+const lista = document.getElementById("lista");   //capitura a lista
 
 form.addEventListener("submit", (evento) => {
-  evento.preventDefault()
-  console.log(evento)
-  console.log(evento.target.elements['nome'].value)
-  console.log(evento.target.elements['quantidade'].value)
-})
+  evento.preventDefault();
+
+ criaElemento(evento.target.elements["nome"].value,evento.target.elements["quantidade"].value)
+ const quantidade = evento.target.elements['quantidade'].value;  
+  
+});
+
+function criaElemento(nome, quantidade) {
+  
+  const novoItem = document.createElement("li"); //cria novo item e adiciona na li
+  novoItem.classList.add("item");
+
+  const numeroItem = document.createElement('strong'); // Insere uma tag strong para o número
+  numeroItem.innerHTML = quantidade; // A quantidade do item fica dentro do HTML
+  
+  const nomeItem = document.createElement('span'); // Cria um elemento para o nome do item
+  nomeItem.innerHTML = nome; // Define o valor do nome do item
+  
+  novoItem.appendChild(numeroItem); // Adiciona, dentro da tag strong, o número do item
+  novoItem.appendChild(nomeItem); // Adiciona o elemento do nome do item
+  
+  lista.appendChild(novoItem);
+
+}
