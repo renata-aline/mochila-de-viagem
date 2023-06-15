546309c783af1221ba0e96dcb7aa65b69b2b79a3
renata-aline <renataaline@gmail.com>
2023-06-15 17:27:24 -0300

    modificando quantidade de item

--- js/main.js
+++ js/main.js
@@ -1,53 +1,62 @@
 const form = document.getElementById("novoItem"); //captura formulário
-const lista = document.getElementById("lista");   //capitura a lista
-const itens = JSON.parse(localStorage.getItem("itens"))|| []
+const lista = document.getElementById("lista"); //capitura a lista
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-
+// for forEach() mantem os dados na página
 itens.forEach((elemento) => {
-  criaElemento(elemento)
-})
+  criaElemento(elemento);
+});
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
 
-  const nome = evento.target.elements['nome']
-  const quantidade = evento.target.elements['quantidade']
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
+
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
 
   const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  if (existe) {
+    itemAtual.id = existe.id;
+
+    atualizaElemento(itemAtual)
+
+  } else {
+    itemAtual.id = itens.length;
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual); //incerir elemento no array
   }
-  
 
-  criaElemento(itemAtual)
+  localStorage.setItem("itens", JSON.stringify(itens));
 
-
-  itens.push(itemAtual)     //incerir elemento no array
-
-  localStorage.setItem("itens", JSON.stringify(itens))  
-
-
-  nome.value = ""    //esvaziar formulário
-  quantidade.value = ""
-  
+  nome.value = ""; //esvaziar formulário
+  quantidade.value = "";
 });
 
 function criaElemento(item) {
-  
   const novoItem = document.createElement("li"); //cria novo item e adiciona na li
   novoItem.classList.add("item");
 
-  const numeroItem = document.createElement('strong'); // Insere uma tag strong para o número
+  const numeroItem = document.createElement("strong"); // Insere uma tag strong para o número
   numeroItem.innerHTML = item.quantidade; // A quantidade do item fica dentro do HTML
-  
-  const nomeItem = document.createElement('span'); // Cria um elemento para o nome do item
+
+  numeroItem.dataset.id = item.id;
+
+  const nomeItem = document.createElement("span"); // Cria um elemento para o nome do item
   nomeItem.innerHTML = item.nome; // Define o valor do nome do item
-  
+
   novoItem.appendChild(numeroItem); // Adiciona, dentro da tag strong, o número do item
   novoItem.appendChild(nomeItem); // Adiciona o elemento do nome do item
-  
+
   lista.appendChild(novoItem);
+}
 
- 
-
+function atualizaElemento(item){
+  document.querySelector("[data-id'"=+item.id+"']").innerHTML = item.quantidade
 }
